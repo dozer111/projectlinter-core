4fb9abe39d653c5b3b25bb2890332d6fa0619195
dozer111 <alkhonko@gmail.com>
2024-07-16 14:51:12 +0000

    :sparkles: (PHP composer) - add new rule "Section platform has only specified values"

---
+++ rules/php/composer/rule/config/platform/only_specified_values_exists.go
@@ -0,0 +1,65 @@
+package platform
+
+import (
+	"github.com/dozer111/projectlinter-core/printer"
+	"github.com/dozer111/projectlinter-core/rules"
+)
+
+// OnlySpecifiedPlatformExistsRule Вказує що "config.platform" має містити ВИКЛЮЧНО вказані платформи
+type OnlySpecifiedPlatformExistsRule struct {
+	expectedValues map[string]string
+	value          map[string]string
+	isPassed       bool
+	isValidated    bool
+}
+
+var _ rules.Rule = (*OnlySpecifiedPlatformExistsRule)(nil)
+
+func NewOnlySpecifiedPlatformExistsRule(
+	platformsInConfig map[string]string,
+	expectedValues map[string]string,
+) *OnlySpecifiedPlatformExistsRule {
+	return &OnlySpecifiedPlatformExistsRule{
+		value:          platformsInConfig,
+		expectedValues: expectedValues,
+	}
+}
+
+func (r *OnlySpecifiedPlatformExistsRule) ID() string {
+	return "composer.config.platform.only_specified.exists"
+}
+
+func (r *OnlySpecifiedPlatformExistsRule) Title() string {
+	return `"config.platform" has only specified values`
+}
+
+func (r *OnlySpecifiedPlatformExistsRule) Validate() {
+	value := r.value
+
+	if len(value) > len(r.expectedValues) {
+		r.isPassed = false
+		return
+	}
+
+	for platformName := range r.expectedValues {
+		if _, ok := (value)[platformName]; !ok {
+			r.isPassed = false
+			return
+		}
+	}
+
+	r.isPassed = true
+}
+
+func (r *OnlySpecifiedPlatformExistsRule) IsPassed() bool {
+	return r.isPassed
+}
+
+func (r *OnlySpecifiedPlatformExistsRule) FailedMessage() []string {
+	return printer.NewCodeHintPrinter(
+		nil,
+		platformPrettyJson(r.value),
+		platformPrettyJson(r.expectedValues),
+		nil,
+	).Print()
+}
