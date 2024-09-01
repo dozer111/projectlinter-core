ee61dd7228ee377798cc1062f39da2dbfbcdcc88
dozer111 <alkhonko@gmail.com>
2024-09-01 17:21:47 +0300

    ✨ dependency: add support js npm

---
+++ rules/dependency/dependency_js_npm_test.go
@@ -0,0 +1,26 @@
+package dependency_test
+
+import (
+	"testing"
+
+	"github.com/dozer111/projectlinter-core/util/path_provider"
+	utilTest "github.com/dozer111/projectlinter-core/util/test"
+
+	"github.com/dozer111/projectlinter-core/rules/dependency"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestJavascriptNPMDependencySet(t *testing.T) {
+	pathProvider := path_provider.NewPathProvider(utilTest.PathInProjectLinter("testdata/js_npm"))
+
+	s := dependency.NewJavascriptNPMDependencySet(pathProvider, nil, nil)
+	errs := s.Init()
+	assert.Equal(t, 0, len(errs))
+
+	rules := s.Run().Resolve([]string{})
+
+	assert.Equal(t, 2, len(rules))
+	failRules := utilTest.AllSetRulesArePassed(rules)
+	assert.True(t, len(failRules) == 0, "Some rules are failed:\n%v", failRules)
+}
