b8519655e00435fb5ec1f15dd5b390350aa9097e
dozer111 <alkhonko@gmail.com>
2025-01-22 10:01:15 +0200

    (php|composer) improve rule `section is absent`
    now it supports `BoolString` value type

--- rules/php/composer/rule/section_is_absent_test.go
+++ rules/php/composer/rule/section_is_absent_test.go
@@ -1,64 +1,68 @@
 package rule_test
 
 import (
-	"github.com/dozer111/projectlinter-core/rules/php/composer/rule"
 	"testing"
+
+	composerCustomType "github.com/dozer111/projectlinter-core/rules/php/composer/config/composer_json/type"
+	"github.com/dozer111/projectlinter-core/rules/php/composer/rule"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestSectionIsAbsentPassedWhenSectionIsAbsent(t *testing.T) {
-	var str *string
+	t.Run("should pass on nil", func(t *testing.T) {
+		var value *string
+		r := rule.NewSectionIsAbsentRule("type", value)
+		r.Validate()
 
-	cases := []struct {
-		description string
-		value       *string
-	}{
-		{
-			"nil",
-			nil,
-		},
-		{
-			"var val *string",
-			str,
-		},
-	}
+		assert.True(t, r.IsPassed())
+	})
 
-	for _, testCase := range cases {
-		t.Run(testCase.description, func(t *testing.T) {
-			r := rule.NewSectionIsAbsentRule("type", testCase.value)
+	t.Run("should fail on", func(t *testing.T) {
+		cases := map[string]any{
+			// yes, it is zero value, but its not nil
+			"empty string":     "",
+			"not-empty string": "vailkamu",
+			"boolstring":       composerCustomType.BoolString{StrVal: "mirage"},
+		}
+
+		for description, value := range cases {
+			t.Run(description, func(t *testing.T) {
+				r := rule.NewSectionIsAbsentRule("type", &value)
+				r.Validate()
+
+				assert.False(t, r.IsPassed())
+			})
+		}
+	})
+
+	t.Run("should handle type on fail", func(t *testing.T) {
+		t.Run("string", func(t *testing.T) {
+			strVal := ""
+			r := rule.NewSectionIsAbsentRule("type", &strVal)
 			r.Validate()
-
-			assert.True(t, r.IsPassed())
+			assert.NotPanics(t, func() { r.FailedMessage() })
 		})
-	}
-}
 
-func TestSectionIsAbsentFailedWhenSectionExists(t *testing.T) {
-	stringToPointer := func(val string) *string {
-		return &val
-	}
-
-	cases := []struct {
-		description string
-		value       *string
-	}{
-		{
-			"empty string",
-			stringToPointer(""),
-		},
-		{
-			"not-empty string",
-			stringToPointer("vailkau"),
-		},
-	}
-
-	for _, testCase := range cases {
-		t.Run(testCase.description, func(t *testing.T) {
-			r := rule.NewSectionIsAbsentRule("type", testCase.value)
+		t.Run("bool", func(t *testing.T) {
+			boolVal := false
+			r := rule.NewSectionIsAbsentRule("type", &boolVal)
 			r.Validate()
-
-			assert.False(t, r.IsPassed())
+			assert.NotPanics(t, func() { r.FailedMessage() })
 		})
-	}
+
+		t.Run("boolString bool", func(t *testing.T) {
+			boolStringBoolVal := composerCustomType.BoolString{BoolVal: false, IsBool: true}
+			r := rule.NewSectionIsAbsentRule("type", &boolStringBoolVal)
+			r.Validate()
+			assert.NotPanics(t, func() { r.FailedMessage() })
+		})
+
+		t.Run("boolString string", func(t *testing.T) {
+			boolStringStringVal := composerCustomType.BoolString{BoolVal: false, IsBool: true}
+			r := rule.NewSectionIsAbsentRule("type", &boolStringStringVal)
+			r.Validate()
+			assert.NotPanics(t, func() { r.FailedMessage() })
+		})
+	})
 }
